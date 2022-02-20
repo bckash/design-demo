42355dc622de3b1512fbba93708316f7e7d1d479
bart c <kaszikasz@wp.pl>
2022-02-20 15:28:45 +0000

    full site upload v1.0

--- app.js
+++ app.js
@@ -1,36 +1,51 @@
 
-// const
+// CONST
+//-------
 
+// buttons round
 const butWearBlack = document.getElementById("w-b");
 const butWearWhite = document.getElementById("w-w");
 const butWearDes = document.getElementById("w-des");
 const butWearDet = document.getElementById("w-det");
+
+// nav up
 const butNav = document.getElementById("nav-up--but")
-
-const secWear = document.getElementsByClassName("wear-js");
-
 const ulNavUp = document.getElementById("n-up-main");
 const ulNavUpChildren = document.getElementsByClassName("um");
-const dodWearCont = document.getElementById("dod-wear__cont");
 
+// "dod wear" sections
+const secWear = document.getElementsByClassName("wear-js");
+
+// "main menu" content containers
+const dodWearCont = document.getElementById("dod-wear__cont");
+const analogiaCont = document.getElementById("analogia__cont");
+const pdecCont = document.getElementById("pdec__cont");
+const brgCont = document.getElementById("brg__cont");
+const bcCont = document.getElementById("bc__cont");
+const wfdCont = document.getElementById("wfd__cont");
+
+// for "next" (F)
 let gNextindex = 0;
 let currentNE = ulNavUp.firstElementChild;
 let currentNEall = Array.from(ulNavUpChildren);
 
 
 // EL
+//-------
 
 butWearBlack.addEventListener("click", function(){turnOn(secWear, "dw-black")});
 butWearWhite.addEventListener("click", function(){turnOn(secWear, "dw-white")});
 butWearDes.addEventListener("click", function(){turnOn(secWear, "dw-design")});
 butWearDet.addEventListener("click", function(){turnOn(secWear, "dw-detail")});
 
-butNav,addEventListener("click", goNext)
+butNav.addEventListener("click", goNext)
 
 
 // () =>
+//-------
 
-// round buttons in "dod wear"
+
+// ROUND BUTTONS in "dod wear"
 function turnOn(collection, ID) {
     
     const arr = Array.from(collection);
@@ -45,7 +60,7 @@ function turnOn(collection, ID) {
     } 
 }
 
-// chevron button
+// CHEVRON BUTTON: add index
 function goNext (e) {
     e.preventDefault()
     gNextindex += 1;
@@ -53,17 +68,27 @@ function goNext (e) {
     e.preventDefault()
 }
 
+// CHEVRON BUTTON: change pictures
 function next(ev) {
 
-    ev.preventDefault()
-
-    if (currentNE.id === "pdec") {
-        dodWearCont.style.display = "block"
-    } else {
-        dodWearCont.style.display = "none"
+    // (F) on/off for main menu titles
+    function onOff (idName, contName){
+        
+        if (currentNE.id === idName) {
+            contName.style.display = "block"
+        } else {
+            contName.style.display = "none" 
     }
+}
 
-    // curent "Li" visible, rest NOT
+    onOff("wfd", dodWearCont)
+    onOff("dod", analogiaCont)
+    onOff("analogia", pdecCont)
+    onOff("pdec", brgCont)
+    onOff("brg", bcCont)
+    onOff("bc", wfdCont)
+
+    // (F) curent "Li" visible, rest NOT
     function displayChange (a){
 
         for (let y = 0; y < currentNEall.length; y++) {
@@ -80,7 +105,6 @@ function next(ev) {
         gNextindex = 0;
         after = currentNEall[0];
         displayChange(after); 
-
         currentNE = after;
         return currentNE;
     } 
@@ -89,11 +113,8 @@ function next(ev) {
 
         let after = currentNE.nextElementSibling
         displayChange(after);
-
         currentNE = after;
         return currentNE;
     }
 
-
-    console.log("-----------")
 }
